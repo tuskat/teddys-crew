25af6d2b7a420ab0bfcab5e7bfeec5b94bdb03f5
xv-cedric-m <cedric.m@expressvpn.com>
2019-09-03 10:52:07 +0800

    tweak readme/mvp

---
+++ src/games/cadre-de-guerre/configs/enums/eventList.ts
@@ -0,0 +1,11 @@
+export enum eventList {
+  Respawn = 'playerRespawned',
+  LifeUpdate = 'lifeUpdate',
+  Damaged = 'entityDamaged',
+  Dashing = 'entityDashing',
+  Shooting = 'entityShooting',
+  Dying = 'entityDied',
+  CountDown ='countDown',
+  RoundEnded = 'roundEnded',
+  LevelUp = 'levelUp'
+};
